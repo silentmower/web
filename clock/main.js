d91c4ca691fe01fc42d51b5f801a28ca1a3c1ef4
filip <filips.zawada@gmail.com>
2025-06-17 20:55:45 +0200

    clock

---
+++ clock/main.js
@@ -0,0 +1,14 @@
+let date = new Date();
+
+let day = date.getDate();
+
+let month = date.getMonth()+1;
+
+letyear = date.GetFullYear();
+
+let hours = date.GetHours();
+
+let minutes = date.GetMinutes();
+
+let secounds = date.getSeconds();
+
