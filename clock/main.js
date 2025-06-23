44056e7c1611c32f9460142feb010fa918acd3c0
Filip <106113670+silentmower@users.noreply.github.com>
2025-06-23 23:10:17 +0200

    Update main.js

--- clock/main.js
+++ clock/main.js
@@ -27,5 +27,34 @@ setTimeout(refresh, 1000);
 
 }
 
+function refresh(){
+
+let date = new Date();
+
+let day = date.getDate();
+if (day < 10){day = "0" + day}
+
+let month = date.getMonth() + 1;
+if (month < 11){month= "0" + month}
+
+let year = date.getFullYear();
+
+let hours = date.getHours();
+if (hours < 10){hours = "0" + hours}
+
+let minutes = date.getMinutes();
+if (minutes < 10){minutes = "0" + minutes}
+
+let secounds = date.getSeconds();
+if (secounds < 10){secounds = "0" + secounds}
+
+
+document.getElementById("clock").innerHTML = hours + ":" + minutes + ":" + secounds + "<br>" + year + "-" + month + "-" + day;
+
+
+setTimeout(refresh, 1000);
+
+}
+
 
 
